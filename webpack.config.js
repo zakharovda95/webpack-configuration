6c34ec7a83f3dfc41f3ed47202e17e21ff69c753
@zakharovda95 <zakharovda95@gmail.com>
2023-03-18 12:45:20 +0300

    sass less preprocessors

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
-console.log(isDev);
+const isProd = !isDev;
+
+// вынесли объект оптимизации в функцию чтобы определить среду разработки и расширить
+const optimization = () => {
+    // дефолтный конфиг поля оптимизации вебпака
+    const conf = {
+        splitChunks: {
+            chunks: 'all',
+        }
+    }
+
+    // если продакшен добавляем минимайзеры, в девелопе - нельзя
+    if (isProd) {
+        conf.minimizer = [new OptimizeCSSAssetsWebpackPlugin(), new TerserWebpackPlugin()]
+    }
+
+    return conf;
+}
+
+// паттерн файлнейма: в дев только имя, в проде имя + хэш
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 
 module.exports = {
     // корневая исходников
@@ -18,7 +41,7 @@ module.exports = {
 
     // точка выхода
     output: {
-        filename: '[name].[hash].js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist'),
     },
 
@@ -33,11 +56,7 @@ module.exports = {
     },
 
     // выносит общий код в vendor-чанк
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        }
-    },
+    optimization: optimization(),
 
     // автозапуск и авторелоадинг с помощью плагина вебпак-дев-сервер
     devServer: {
@@ -48,7 +67,11 @@ module.exports = {
     //подключение плагинов, перенос ХТМЛ из срк в дист и очистка старых чанков
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            // минификация хтмл
+            minify: {
+                collapseWhitespace: isProd,
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
@@ -56,12 +79,15 @@ module.exports = {
                 {
                     from: path.resolve(__dirname, 'public'),
                     to: path.resolve(__dirname, 'dist'),
+                    noErrorOnMissing: true,
                 }
             ]
         }),
         new MiniCSSExtractPlugin({
-            filename: '[name].[hash].css',
-        })
+            filename: filename('css'),
+        }),
+        new OptimizeCSSAssetsWebpackPlugin(),
+        new TerserWebpackPlugin(),
     ],
 
     // парсинг стилей картинок
@@ -69,14 +95,15 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCSSExtractPlugin.loader,
-                        options: {
-                            hmr: isDev,
-                            reloadAll: true
-                        }
-                    }, 'css-loader']
+                use: [MiniCSSExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'less-loader']
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
