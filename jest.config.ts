b195dc88681e361703f7962c1ac70a0f5f58faf2
@zakharovda95 <zakharovda95@gmail.com>
2023-03-21 10:27:23 +0300

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,4 @@ export default {
   clearMocks: false,
   collectCoverage: false,
   testEnvironment: 'jsdom',
-  verbose: true,
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
-  },
 };
